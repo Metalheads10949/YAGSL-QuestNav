8374bc450f1a5920919b628783fea1666e8823b4
AceTheAro <acecostello07@gmail.com>
2025-11-11 01:38:53 -0500

    mostly just digging around, i made a questnav imu class but i need to figure out how to actually make it do anything

---
+++ swervelib/imu/QuestNavSwerve.java
@@ -0,0 +1,15 @@
+package swervelib.imu;
+
+import static edu.wpi.first.units.Units.DegreesPerSecond;
+
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.units.measure.LinearAcceleration;
+import edu.wpi.first.units.measure.MutAngularVelocity;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import java.util.Optional;
+import java.util.function.Supplier;
+
+public class QuestNaveSwerve extends SwerveIMU {
+
+}
