873a01b4f6a80a1350024fda940dc212a4a222ff
AceTheAro <acecostello07@gmail.com>
2025-11-13 12:47:12 -0500

    temporarily Commented class QuestNavSwerve

--- swervelib/imu/QuestNavSwerve.java
+++ swervelib/imu/QuestNavSwerve.java
@@ -10,6 +10,4 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import java.util.Optional;
 import java.util.function.Supplier;
 
-public class QuestNavSwerve extends SwerveIMU {
-
-}
+//public class QuestNavSwerve extends SwerveIMU {}
